89af00961b78be2bd2b4954cf8662bed46bb0a1d
gbubemi22 <slimnfine22@gmail.com>
2024-06-21 14:47:50 +0100

    setup

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,53 +1,138 @@
 package database
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-	"time"
+    "context"
+    "fmt"
+    "log"
+    "os"
+    "time"
 
-	_ "github.com/joho/godotenv/autoload"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Service interface {
-	Health() map[string]string
+var globalClient *mongo.Client
+
+func init() {
+    client, err := DBinstance()
+    if err != nil {
+        log.Fatalf("Failed to initialize MongoDB client: %v", err)
+    }
+    globalClient = client
 }
 
-type service struct {
-	db *mongo.Client
+func DBinstance() (*mongo.Client, error) {
+    MongoDb := os.Getenv("DB_DATABASE")
+    client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+    if err != nil {
+        return nil, fmt.Errorf("failed to create MongoDB client: %w", err)
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    err = client.Connect(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+    }
+
+    err = client.Ping(ctx, nil)
+    if err != nil {
+        return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+    }
+
+    fmt.Println("Connected to MongoDB")
+    return client, nil
 }
 
-var (
-	host = os.Getenv("DB_HOST")
-	port = os.Getenv("DB_PORT")
-	//database = os.Getenv("DB_DATABASE")
-)
-
-func New() Service {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s", host, port)))
-
-	if err != nil {
-		log.Fatal(err)
-
-	}
-	return &service{
-		db: client,
-	}
+type Service struct {
+    db *mongo.Client
 }
 
-func (s *service) Health() map[string]string {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	err := s.db.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf(fmt.Sprintf("db down: %v", err))
-	}
-
-	return map[string]string{
-		"message": "It's healthy",
-	}
+func New() (*Service, error) {
+    client, err := DBinstance()
+    if err != nil {
+        return nil, fmt.Errorf("failed to initialize service: %w", err)
+    }
+    return &Service{db: client}, nil
 }
+
+func (s *Service) Health() map[string]string {
+    ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+    defer cancel()
+
+    err := s.db.Ping(ctx, nil)
+    if err != nil {
+        log.Fatalf("db down: %v", err)
+    }
+
+    return map[string]string{
+        "message": "It's healthy",
+    }
+}
+
+// OpenCollection opens a MongoDB collection
+func (s *Service) OpenCollection(collectionName string) *mongo.Collection {
+    collection := s.db.Database("GO_Rental").Collection(collectionName)
+    return collection
+}
+
+
+
+// package database
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	//"log"
+// 	"os"
+// 	"time"
+
+// 	_ "github.com/joho/godotenv/autoload"
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// type Service interface {
+// 	Health() map[string]string
+// }
+
+// type service struct {
+// 	db *mongo.Client
+// }
+
+
+
+// func DBinstance() (*mongo.Client, error) {
+
+// 	MongoDb := os.Getenv("DB_DATABASE")
+// 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to create MongoDB client: %w", err)
+// 	}
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+// 	}
+
+// 	err = client.Ping(ctx, nil)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+// 	}
+
+// 	fmt.Println("Connected to MongoDB")
+// 	return client, nil
+// }
+
+// // OpenCollection opens a MongoDB collection
+// func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+// 	collection := client.Database("GO_Rental").Collection(collectionName)
+// 	return collection
+// }
+
+
+
--- internal/server/server.go
+++ internal/server/server.go
@@ -14,26 +14,37 @@ import (
 
 type Server struct {
 	port int
-
-	db database.Service
-}
-
-func NewServer() *http.Server {
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
-		port: port,
-
-		db: database.New(),
+	db   *database.Service
+ }
+ 
+ func NewServer() *http.Server {
+	portStr := os.Getenv("PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+	    port = 8080 
 	}
-
+ 
+	db, err := database.New()
+	if err != nil {
+	    
+	    fmt.Printf("Error initializing database: %v\n", err)
+	  
+	}
+ 
+	NewServer := &Server{
+	    port: port,
+	    db:   db,
+	}
+ 
 	// Declare Server config
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	    Addr:         fmt.Sprintf(":%d", NewServer.port),
+	    Handler:      NewServer.RegisterRoutes(),
+	    IdleTimeout:  time.Minute,
+	    ReadTimeout:  10 * time.Second,
+	    WriteTimeout: 30 * time.Second,
 	}
-
+ 
 	return server
-}
+ }
+ 
