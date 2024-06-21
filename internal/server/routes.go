0f6e3ca55bab255fae60bf2f9638bb73236a3f89
gbubemi22 <slimnfine22@gmail.com>
2024-06-21 13:42:19 +0100

    Initial commit

---
+++ internal/server/routes.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	r.GET("/", s.HelloWorldHandler)
+
+	r.GET("/health", s.healthHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(c *gin.Context) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, s.db.Health())
+}
